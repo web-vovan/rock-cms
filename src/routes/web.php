cc7704b9fc2a5489ea0d6bc4b95c42adc2690339
vladimir <myweb11@mail.ru>
2022-05-25 16:41:47 +0300

    test update

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', function() {
     return view('rock.cms::test');
 });
+
+Route::get('/test2', function() {
+    return view('rock.cms::test2');
+});
