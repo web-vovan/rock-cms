fb7a643ec6656ad15239b594d2f1e5b96c960832
vladimir <myweb11@mail.ru>
2022-05-27 14:44:00 +0300

    переопределил главный шаблон для админки

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -1,13 +1,11 @@
-@extends('adminlte::page')
+@extends('rock-cms::page')
 
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Админ-панель Моризо</h1>
+    <h1>Rock.Cms - простая админка для Laravel.</h1>
 @stop
 
 @section('content')
-    <div class="dashboard-wrap">
-        <img class="logo" src="{{asset('admin-resources/img/logo.svg')}}" alt="">
-    </div>
+
 @stop
---
+++ src/resources/views/page.blade.php
@@ -0,0 +1,58 @@
+@extends('adminlte::master')
+
+@inject('layoutHelper', 'JeroenNoten\LaravelAdminLte\Helpers\LayoutHelper')
+
+@section('adminlte_css')
+    @stack('css')
+    @yield('css')
+@stop
+
+@section('adminlte_css_pre')
+    <link rel="stylesheet" href="{{ asset('admin-resources/css/app.css') }}">
+    <script src="{{ asset('admin-resources/js/app.js') }}"></script>
+@stop
+
+@section('classes_body', $layoutHelper->makeBodyClasses())
+
+@section('body_data', $layoutHelper->makeBodyData())
+
+@section('body')
+    <div class="wrapper">
+
+        {{-- Top Navbar --}}
+        @if($layoutHelper->isLayoutTopnavEnabled())
+            @include('adminlte::partials.navbar.navbar-layout-topnav')
+        @else
+            @include('adminlte::partials.navbar.navbar')
+        @endif
+
+        {{-- Left Main Sidebar --}}
+        @if(!$layoutHelper->isLayoutTopnavEnabled())
+            @include('adminlte::partials.sidebar.left-sidebar')
+        @endif
+
+        {{-- Flash Messages--}}
+        @include('admin.partials.flash-messages')
+
+        {{-- Content Wrapper --}}
+        @empty($iFrameEnabled)
+            @include('adminlte::partials.cwrapper.cwrapper-default')
+        @else
+            @include('adminlte::partials.cwrapper.cwrapper-iframe')
+        @endempty
+
+        {{-- Footer --}}
+        @include('admin.partials.footer')
+
+        {{-- Right Control Sidebar --}}
+        @if(config('adminlte.right_sidebar'))
+            @include('adminlte::partials.sidebar.right-sidebar')
+        @endif
+
+    </div>
+@stop
+
+@section('adminlte_js')
+    @stack('js')
+    @yield('js')
+@stop
