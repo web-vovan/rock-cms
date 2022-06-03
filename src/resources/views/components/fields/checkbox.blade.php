89bcdccc5a1b71e385b238d420ef06c3da9aac2d
vladimir <myweb11@mail.ru>
2022-06-03 09:33:55 +0300

    правка стилей поля с чекбоксом

--- src/resources/views/components/fields/checkbox.blade.php
+++ src/resources/views/components/fields/checkbox.blade.php
@@ -1,8 +1,10 @@
 <div class="form-group row mb-4">
     <label for="{{$field}}" class="col-sm-2 col-form-label">{{ $title }}</label>
     <div class="col-sm-10">
-        <input wire:model="{{ $field }}"
-               type="checkbox"
-               id="{{$field}}">
+        <div class="col-form-label">
+            <input wire:model="{{ $field }}"
+                   type="checkbox"
+                   id="{{$field}}">
+        </div>
     </div>
 </div>
