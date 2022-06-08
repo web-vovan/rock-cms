4dbf2b7822d76b288e8b394281b740c1afa66172
vladimir <myweb11@mail.ru>
2022-06-08 15:58:33 +0300

    добавил трейт для мультиселекта

--- src/Traits/Livewire/HasSortable.php
+++ src/Traits/Livewire/HasSortable.php
@@ -31,38 +31,4 @@ trait HasSortable
             return $this->$field[(int) $index];
         }, $data);
     }
-
-    /**
-     * Добавление элемента в список
-     *
-     * @param string $itemsField
-     * @param string $selectedItemsField
-     * @param $value
-     */
-    public function addItemInSelectedItems(string $itemsField, string $selectedItemsField, $value)
-    {
-        $this->changeSelect($this->$itemsField, $this->$selectedItemsField, $value);
-    }
-
-    /**
-     * Добавление элемента в список из мультиселекта
-     *
-     * @param array $items
-     * @param array $selectedItems
-     * @param string $value
-     */
-    public function changeSelect(array $items, array &$selectedItems, string $value)
-    {
-        $result = null;
-
-        foreach ($items as $key => $item) {
-            if ($item['id'] === (int) $value) {
-                $result = $items[$key];
-            }
-        }
-
-        if ($result) {
-            $selectedItems[] = $result;
-        }
-    }
 }
