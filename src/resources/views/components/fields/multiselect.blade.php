137671f2e278ee0ff39f346fd70a8b9ec33d3679
vladimir <myweb11@mail.ru>
2022-06-09 14:52:18 +0300

    объединил мультиселекты в одно поле

--- src/resources/views/components/fields/multiselect.blade.php
+++ src/resources/views/components/fields/multiselect.blade.php
@@ -1,30 +1,98 @@
-<div class="form-group row mb-4" wire:ignore>
+<div class="form-group row mb-4">
     <label class="col-sm-2 col-form-label">{{ $title }}</label>
     <div class="col-sm-10">
-        <select id="{{ $field }}" class="chosen form-control" data-placeholder="Выберите значение" multiple>
-            @foreach($items as $key => $item)
-                <option
-                    value="{{ $item['id'] }}"
-                    @if(in_array($item['id'], $selected)) selected @endif
-                >{{ $item['title'] }}</option>
-            @endforeach
-        </select>
+        @if(isset($sortable))
+            @if (isset($liveSearch))
+                <div class="rock-multiselect {{$selectedItemsField}} mb-4" wire:ignore.self>
+                    <input class="form-control {{$selectedItemsField}}" placeholder="Выберите значение" type="text" />
+                    <ul class="list-group">
+                        @if(count($items) > 0)
+                            @foreach($items as $key => $item)
+                                <div class="list-group-item list-group-item-action" wire:click="addItemInSelectedItems('{{$itemsField}}', '{{$selectedItemsField}}', {{$item['id']}})" role='button'>{{$item['title']}}</div>
+                            @endforeach
+                        @else
+                            <div class="list-group-item">Ничего не найдено</div>
+                        @endif
+                    </ul>
+                </div>
+            @else
+                <div wire:ignore class="mb-4">
+                    <select id="{{ $selectedItemsField }}" class="chosen form-control" data-placeholder="Выберите значение">
+                        <option value="0"></option>
+                        @foreach($items as $key => $item)
+                            <option
+                                value="{{ $item['id'] }}"
+                            >{{ $item['title'] }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            @endif
+
+            <div wire:sortable="changeOrder">
+                @foreach ($selectedItems as $item)
+                    <div class="input-group mb-3" wire:sortable.item="{{$selectedItemsField}}:{{ $loop->index }}" wire:key="{{$selectedItemsField}}-{{ $loop->index }}">
+                        <div class="input-group-prepend">
+                            <button class="btn btn-primary btn-sm" wire:sortable.handle="{{ $loop->index }}"><i class="fas fa-arrows-alt"></i></button>
+                        </div>
+                        <div class="form-control">{{ $item['title'] }}</div>
+                        <div class="input-group-append">
+                            <div class="btn btn-danger btn-sm" wire:click="removeListItem({{ $loop->index }}, '{{$selectedItemsField}}')"><i class="fas fa-trash"></i></div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        @else
+            <div wire:ignore>
+                <select id="{{ $selectedItemsField }}" class="chosen form-control" data-placeholder="Выберите значение" multiple>
+                    @foreach($items as $key => $item)
+                        <option
+                            value="{{ $item['id'] }}"
+                            @if(in_array($item['id'], $selectedItems)) selected @endif
+                        >{{ $item['title'] }}</option>
+                    @endforeach
+                </select>
+            </div>
+        @endif
     </div>
 </div>
 
 @push('js')
     <script>
-        let chosenBlock = $('#{{$field}}');
+        let {{$selectedItemsField}} = $('#{{$selectedItemsField}}');
+        {{$selectedItemsField}}.chosen();
 
-        chosenBlock.chosen();
+        @if(isset($sortable))
+        @if (isset($liveSearch))
+        $('.rock-multiselect .{{$selectedItemsField}}').on('input', function(event) {
+        @this.{{$liveSearch}}(event.target.value)
+        })
 
-        chosenBlock.change(function() {
-            let select = document.getElementById('{{ $field }}');
+        $('.rock-multiselect .{{$selectedItemsField}}').on('focus', function(event) {
+            $('.rock-multiselect.{{$selectedItemsField}}').addClass('active')
+        })
+
+        $('.rock-multiselect .{{$selectedItemsField}}').on('blur', function(event) {
+            setTimeout(function() {
+                $('.rock-multiselect.{{$selectedItemsField}}').removeClass('active')
+            }, 500)
+        })
+        @else
+        {{ $selectedItemsField }}.change(function() {
+            let select = document.getElementById('{{ $selectedItemsField }}');
+        @this.addItemInSelectedItems('{{$itemsField}}', '{{$selectedItemsField}}', select.value);
+
+            select.value = null;
+        })
+        @endif
+        @else
+        {{$selectedItemsField}}.change(function() {
+            let select = document.getElementById('{{ $selectedItemsField }}');
             let selected = [...select.options]
                 .filter(option => option.selected)
                 .map(option => option.value);
 
-            @this.set('{{ $field }}', selected);
+        @this.set('{{ $selectedItemsField }}', selected);
         })
+        @endif
     </script>
 @endpush()
