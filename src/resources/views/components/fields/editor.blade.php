599c59455b5ea1bf681cd1a7fb2dce75120ffde6
vladimir <myweb11@mail.ru>
2022-06-29 10:46:52 +0300

    фикс в текстовом редакторе

--- src/resources/views/components/fields/editor.blade.php
+++ src/resources/views/components/fields/editor.blade.php
@@ -73,13 +73,27 @@
                 usrCfg['placeholder'] = "{{ $attributes['placeholder'] }}";
             @endisset
 
-                usrCfg['popover'] = {
+            usrCfg['popover'] = {
                 image: [
                     ['custom', ['imageAttributes']],
                     ['imagesize', ['imageSize100', 'imageSize50', 'imageSize25']],
                     ['float', ['floatLeft', 'floatRight', 'floatNone']],
                     ['remove', ['removeMedia']]
                 ],
+                link: [
+                    ['link', ['linkDialogShow', 'unlink']]
+                ],
+                table: [
+                    ['add', ['addRowDown', 'addRowUp', 'addColLeft', 'addColRight']],
+                    ['delete', ['deleteRow', 'deleteCol', 'deleteTable']],
+                ],
+                air: [
+                    ['color', ['color']],
+                    ['font', ['bold', 'underline', 'clear']],
+                    ['para', ['ul', 'paragraph']],
+                    ['table', ['table']],
+                    ['insert', ['link', 'picture']]
+                ]
             }
 
             usrCfg['imageAttributes'] = {
