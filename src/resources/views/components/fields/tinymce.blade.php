cd31961afa7e74993fbfc404f8918ddfa0a7bf10
vladimir <myweb11@mail.ru>
2022-07-08 13:37:48 +0300

    фикс tinymce в модальном окне

--- src/resources/views/components/fields/tinymce.blade.php
+++ src/resources/views/components/fields/tinymce.blade.php
@@ -80,6 +80,12 @@
                 });
             },
         });
+
+        $(document).on('focusin', function(e) {
+            if ($(e.target).closest(".tox-tinymce-aux, .moxman-window, .tam-assetmanager-root").length) {
+                e.stopImmediatePropagation();
+            }
+        });
     </script>
 @empty($ajaxEditor)
     @endpush
